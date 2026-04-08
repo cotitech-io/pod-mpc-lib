b98a221dcc8fe4fbb9d05664e5d8704b1a857dec
Naiem <naiem@coti.io>
2026-04-09 01:35:50 +0300

    Fix the chain id in PodUserSeplia.sol

--- contracts/mpc/PodUserSepolia.sol
+++ contracts/mpc/PodUserSepolia.sol
@@ -7,7 +7,7 @@ import "./PodUser.sol";
 abstract contract PodUserSepolia is PodUser {
 
     address constant INBOX_ADDRESS = 0xFa158f9e49C8bb77f971c3630EbCD23a8a88D14E;
-    uint256 constant COTI_CHAIN_ID = 2632500;
+    uint256 constant COTI_CHAIN_ID = 7082400;
     address constant MPC_EXECUTOR_ADDRESS = 0xC76aaE4F3810fBBd5d96b92DEFeBE0034405Ad9c;
 
     constructor() {
